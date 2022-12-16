81afc70a7d2d266ab46a9b3a2f30ab2fdc4efb75
Your Name <you@example.com>
2022-12-16 15:38:28 +0600

    clean arch

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Levap123/trello-clone
+
+go 1.19
---
+++ pkg/webjson/json.go
@@ -0,0 +1,19 @@
+package webjson
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSONError(w http.ResponseWriter, err error, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+}
+
+func SendJSON(w http.ResponseWriter, input any) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(input)
+}
+
